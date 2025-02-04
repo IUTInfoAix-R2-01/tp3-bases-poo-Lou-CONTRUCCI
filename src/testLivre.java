0c60850d9e50aae2b009d5383bec3ca3dd440427
Lou <lou.contrucci@etu.univ-amu.fr>
2025-02-04 14:19:46 +0100

    Test de la class livre

---
+++ src/testLivre.java
@@ -0,0 +1,12 @@
+
+public class testLivre {
+
+	public static void main(String[] args) {
+		Livre l1 = new Livre("Le Labyrinthe", 28, 5);
+		System.out.println(l1);
+		
+		Livre l2 = new Livre("one piece", 13.99, 12);
+		System.out.println(l2);
+	}
+
+}
