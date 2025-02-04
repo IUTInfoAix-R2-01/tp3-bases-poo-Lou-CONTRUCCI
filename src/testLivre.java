93202703ecc83e91ca221746acd253987243b156
Lou <lou.contrucci@etu.univ-amu.fr>
2025-02-04 14:28:18 +0100

    ajout d'un test pour le setter

--- src/testLivre.java
+++ src/testLivre.java
@@ -7,6 +7,11 @@ public class testLivre {
 		
 		Livre l2 = new Livre("one piece", 13.99, 12);
 		System.out.println(l2);
+		
+		l1.setQuantite(4);
+		l2.setQuantite(8);
+		System.out.println(l1);		
+		System.out.println(l2);
 	}
 
 }
