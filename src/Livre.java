62809e7567d9aa28e83ed4a41fbbe22254476c35
Lou <lou.contrucci@etu.univ-amu.fr>
2025-02-04 14:19:27 +0100

    Class livre

---
+++ src/Livre.java
@@ -0,0 +1,29 @@
+
+public class Livre {
+	private String name ;
+	private double price ;
+	private int quantite ;
+	
+	public Livre(String n, double p, int q) {
+		this.name = n;
+		this.price = p;
+		this.quantite = q;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public int getQuantite() {
+		return quantite;
+	}
+	
+	public String toString() {
+		return "Livre[Name = "+name+", Price = "+price+" € , Quantité = "+quantite+"]";
+	}
+	
+}
